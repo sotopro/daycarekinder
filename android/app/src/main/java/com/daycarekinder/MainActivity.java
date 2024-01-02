130ab573cc9b444c31bec60de5579f86ac22e6a0
Daniel Soto <dasg.1993@gmail.com>
2024-01-02 15:51:30 -0500

    Add dependencies and import statement for navigation

--- android/app/src/main/java/com/daycarekinder/MainActivity.java
+++ android/app/src/main/java/com/daycarekinder/MainActivity.java
@@ -1,3 +1,4 @@
+import android.os.Bundle;
 package com.daycarekinder;
 
 import com.facebook.react.ReactActivity;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "daycarekinder";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
